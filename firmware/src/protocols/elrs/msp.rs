6bacc1922591a55cb5dd633657741e9f0153ebdb
David O'Connor <david.alan.oconnor@gmail.com>
2022-04-18 20:49:34 -0400

    Added MSP code

--- firmware/src/protocols/elrs/msp.rs
+++ firmware/src/protocols/elrs/msp.rs
@@ -7,139 +7,128 @@
 //! https://github.com/ExpressLRS/ExpressLRS/blob/master/src/lib/MSP/msp.h
 //! https://github.com/ExpressLRS/ExpressLRS/blob/master/src/lib/MSP/msp.cpp
 
+use defmt::println;
 
-#define MSP_ELRS_FUNC       0x4578 // ['E','x']
+// todo: These are defines; unkonwn type.
+const ELRS_FUNC: u16 =       0x4578; // ['E','x']
 
-#define MSP_SET_RX_CONFIG   45
-#define MSP_VTX_CONFIG      88   //out message         Get vtx settings - betaflight
-#define MSP_SET_VTX_CONFIG  89   //in message          Set vtx settings - betaflight
-#define MSP_EEPROM_WRITE    250  //in message          no param
+const SET_RX_CONFIG: u16 =    45;
+const VTX_CONFIG : u16 =      88 ;  //out message         Get vtx settings - betaflight
+const SET_VTX_CONFIG : u16 =  89 ;  //in message          Set vtx settings - betaflight
+const EEPROM_WRITE : u16 =    250 ; //in message          no param
 
 // ELRS specific opcodes
-#define MSP_ELRS_RF_MODE                    0x06
-#define MSP_ELRS_TX_PWR                     0x07
-#define MSP_ELRS_TLM_RATE                   0x08
-#define MSP_ELRS_BIND                       0x09
-#define MSP_ELRS_MODEL_ID                   0x0A
-#define MSP_ELRS_REQU_VTX_PKT               0x0B
-#define MSP_ELRS_SET_TX_BACKPACK_WIFI_MODE  0x0C
-#define MSP_ELRS_SET_VRX_BACKPACK_WIFI_MODE 0x0D
-#define MSP_ELRS_SET_RX_WIFI_MODE           0x0E
+const ELRS_RF_MODE    : u16 =                 0x06;
+const ELRS_TX_PWR     : u16 =                 0x07;
+const ELRS_TLM_RATE    : u16 =                0x08;
+const ELRS_BIND       : u16 =                 0x09;
+const ELRS_MODEL_ID     : u16 =               0x0A;
+const ELRS_REQU_VTX_PKT    : u16 =            0x0B;
+const ELRS_SET_TX_BACKPACK_WIFI_MODE: u16 =   0x0C;
+const ELRS_SET_VRX_BACKPACK_WIFI_MODE: u16 =  0x0D;
+const ELRS_SET_RX_WIFI_MODE    : u16 =        0x0E;
 
-#define MSP_ELRS_POWER_CALI_GET             0x20
-#define MSP_ELRS_POWER_CALI_SET             0x21
+const ELRS_POWER_CALI_GET    : u16 =          0x20;
+const ELRS_POWER_CALI_SET    : u16 =          0x21;
 
 // CRSF encapsulated msp defines
-#define ENCAPSULATED_MSP_HEADER_CRC_LEN     4
-#define ENCAPSULATED_MSP_MAX_PAYLOAD_SIZE   4
-#define ENCAPSULATED_MSP_MAX_FRAME_LEN      (ENCAPSULATED_MSP_HEADER_CRC_LEN + ENCAPSULATED_MSP_MAX_PAYLOAD_SIZE)
+const ENCAPSULATED_HEADER_CRC_LEN  : u16 =    4;
+const ENCAPSULATED_MAX_PAYLOAD_SIZE : u16 =   4;
+const ENCAPSULATED_MAX_FRAME_LEN   : u16 =    ENCAPSULATED_HEADER_CRC_LEN + ENCAPSULATED_MAX_PAYLOAD_SIZE;
 
-#pragma once
 
-#include "targets.h"
-
-// TODO: MSP_PORT_INBUF_SIZE should be changed to
+// TODO: PORT_INBUF_SIZE should be changed to
 // dynamically allocate array length based on the payload size
 // Hardcoding payload size to 8 bytes for now, since MSP is
 // limited to a 4 byte payload on the BF side
-#define MSP_PORT_INBUF_SIZE 8
+const PORT_INBUF_SIZE: usize =  8; // todo: define type?
 
-#define CHECK_PACKET_PARSING() \
-  if (packet->readError) {\
-    return;\
-  }
+// #define CHECK_PACKET_PARSING() \
+//   if (packet->readError) {\
+//     return;\
+//   }
 
-typedef enum {
-    MSP_IDLE,
-    MSP_HEADER_START,
-    MSP_HEADER_X,
+#[derive(Clone, Copy, PartialEq)]
+enum State {
+    IDLE,
+    HEADER_START,
+    HEADER_X,
 
-    MSP_HEADER_V2_NATIVE,
-    MSP_PAYLOAD_V2_NATIVE,
-    MSP_CHECKSUM_V2_NATIVE,
+    HEADER_V2_NATIVE,
+    PAYLOAD_V2_NATIVE,
+    CHECKSUM_V2_NATIVE,
 
-    MSP_COMMAND_RECEIVED
-} mspState_e;
+    COMMAND_RECEIVED
+}
 
-typedef enum {
-    MSP_PACKET_UNKNOWN,
-    MSP_PACKET_COMMAND,
-    MSP_PACKET_RESPONSE
-} mspPacketType_e;
+#[derive(Clone, Copy, PartialEq)]
+enum PacketType {
+    UNKNOWN,
+    COMMAND,
+    RESPONSE
+}
 
-typedef struct __attribute__((packed)) {
-    uint8_t  flags;
-    uint16_t function;
-    uint16_t payloadSize;
-} mspHeaderV2_t;
+// typedef struct __attribute__((packed)) { // todo: Anything special to pack?
+pub struct HeaderV2 {
+    flags: u8,
+    function: u16,
+    payloadSize: u16,
+}
 
-typedef struct {
-    mspPacketType_e type;
-    uint8_t         flags;
-    uint16_t        function;
-    uint16_t        payloadSize;
-    uint8_t         payload[MSP_PORT_INBUF_SIZE];
-    uint16_t        payloadReadIterator;
-    bool            readError;
+pub struct Packet {
+    type_: PacketType,
+    flags: u8,
+    function: u16,
+    payloadSize: usize,
+    payload: [u8; PORT_INBUF_SIZE],
+    payloadReadIterator: usize,
+    readError: bool,
+}
 
-    void reset()
-    {
-        type = MSP_PACKET_UNKNOWN;
-        flags = 0;
-        function = 0;
-        payloadSize = 0;
-        payloadReadIterator = 0;
-        readError = false;
+impl Packet {
+    fn reset(&mut self)    {
+        self.type_ = PacketType::UNKNOWN;
+        self.flags = 0;
+        self.function = 0;
+        self.payloadSize = 0;
+        self.payloadReadIterator = 0;
+        self.readError = false;
     }
 
-    void addByte(uint8_t b)
-    {
-        payload[payloadSize++] = b;
+    fn addByte(&mut self, b: u8) {
+        self.payloadSize += 1;
+        self.payload[payloadSize] = b;
     }
 
-    void makeResponse()
-    {
-        type = MSP_PACKET_RESPONSE;
+    fn makeResponse(&mut self)    {
+        self.type_ = PacketType::RESPONSE;
     }
 
-    void makeCommand()
-    {
-        type = MSP_PACKET_COMMAND;
+    fn makeCommand(&mut self)    {
+        self.type_ = PacketType::COMMAND;
     }
 
-    uint8_t readByte()
-    {
-        if (payloadReadIterator >= payloadSize) {
+    fn readByte(&mut self) -> u8    {
+        if self.payloadReadIterator >= self.payloadSize {
             // We are trying to read beyond the length of the payload
-            readError = true;
+            self.readError = true;
             return 0;
         }
 
-        return payload[payloadReadIterator++];
+        self.payloadReadIterator += 1;
+        payload[payloadReadIterator]
     }
-} mspPacket_t;
+}
 
 /////////////////////////////////////////////////
 
-class MSP
-{
-public:
-    bool            processReceivedByte(uint8_t c);
-    mspPacket_t*    getReceivedPacket();
-    void            markPacketReceived();
-    static bool     sendPacket(mspPacket_t* packet, Stream* port);
+struct MSP {
+    inputState: State,
+    offset: u16,
+    inputBuffer: [u8; PORT_INBUF_SIZE],
+    crc: u8,
+}
 
-private:
-    mspState_e  m_inputState;
-    uint16_t    m_offset;
-    uint8_t     m_inputBuffer[MSP_PORT_INBUF_SIZE];
-    mspPacket_t m_packet;
-    uint8_t     m_crc;
-};
-
-#include "msp.h"
-
-#include "logging.h"
 
 /* ==========================================
 MSP V2 Message Structure:
@@ -157,186 +146,182 @@ n+8     checksum                uint8, (n= payload size), crc8_dvb_s2 checksum
 
 // CRC helper function. External to MSP class
 // TODO: Move all our CRC functions to a CRC lib
-uint8_t crc8_dvb_s2(uint8_t crc, unsigned char a)
-{
-    crc ^= a;
-    for (int ii = 0; ii < 8; ++ii) {
-        if (crc & 0x80) {
+fn crc8_dvb_s2(crc: u8, a: char) -> u8 {
+    // crc ^= a;
+    let mut crc = crc ^ a;
+    for i in 0..8 {
+        if crc & 0x80 != 0 {
             crc = (crc << 1) ^ 0xD5;
         } else {
             crc = crc << 1;
         }
     }
-    return crc;
+    crc
 }
 
-bool
-MSP::processReceivedByte(uint8_t c)
-{
-    switch (m_inputState) {
+impl MSP {
 
-        case MSP_IDLE:
-            // Wait for framing char
-            if (c == '$') {
-                m_inputState = MSP_HEADER_START;
+
+    fn processReceivedByte(&mut self, c: u8) -> bool {
+        match self.inputState {
+            State::IDLE => {
+                // Wait for framing char
+                if c == '$' {
+                    self.inputState = State::HEADER_START;
+                }
             }
-            break;
-
-        case MSP_HEADER_START:
-            // Waiting for 'X' (MSPv2 native)
-            switch (c) {
-                case 'X':
-                    m_inputState = MSP_HEADER_X;
-                    break;
-                default:
-                    m_inputState = MSP_IDLE;
-                    break;
+            State::HEADER_START => {
+                // Waiting for 'X' (MSPv2 native)
+                match c {
+                    'X' => {
+                        self.inputState = State::HEADER_X;
+                    }
+                    _ => {
+                        self.inputState = State::IDLE;
+                    }
+                }
             }
-            break;
+            State::HEADER_X => {
+                // Wait for the packet type (cmd or req)
+                self.inputState = State::HEADER_V2_NATIVE;
 
-        case MSP_HEADER_X:
-            // Wait for the packet type (cmd or req)
-            m_inputState = MSP_HEADER_V2_NATIVE;
+                // Start of a new packet
+                // reset the packet, offset iterator, and CRC
+                self.packet.reset();
+                self.offset = 0;
+                self.crc = 0;
 
-            // Start of a new packet
-            // reset the packet, offset iterator, and CRC
-            m_packet.reset();
-            m_offset = 0;
-            m_crc = 0;
-
-            switch (c) {
-                case '<':
-                    m_packet.type = MSP_PACKET_COMMAND;
-                    break;
-                case '>':
-                    m_packet.type = MSP_PACKET_RESPONSE;
-                    break;
-                default:
-                    m_packet.type = MSP_PACKET_UNKNOWN;
-                    m_inputState = MSP_IDLE;
-                    break;
+                match c {
+                    '<' => {
+                        self.packet.type_ = PacketType::COMMAND;
+                    }
+                    '>' => {
+                        self.packet.type_ = PacketType::RESPONSE;
+                    }
+                    _ => {
+                        self.packet.type_ = PacketType::UNKNOWN;
+                        self.inputState = State::IDLE;
+                    }
+                }
             }
-            break;
 
-        case MSP_HEADER_V2_NATIVE:
-            // Read bytes until we have a full header
-            m_inputBuffer[m_offset++] = c;
-            m_crc = crc8_dvb_s2(m_crc, c);
+            State::HEADER_V2_NATIVE => {
+                // Read bytes until we have a full header
+                self.offset += 1;
+                self.inputBuffer[self.offset] = c;
+                self.crc = crc8_dvb_s2(self.crc, c);
 
-            // If we've received the correct amount of bytes for a full header
-            if (m_offset == sizeof(mspHeaderV2_t)) {
-                // Copy header values into packet
-                mspHeaderV2_t* header = (mspHeaderV2_t*)&m_inputBuffer[0];
-                m_packet.payloadSize = header->payloadSize;
-                m_packet.function = header->function;
-                m_packet.flags = header->flags;
-                // reset the offset iterator for re-use in payload below
-                m_offset = 0;
-                m_inputState = MSP_PAYLOAD_V2_NATIVE;
+                // If we've received the correct amount of bytes for a full header
+                if self.offset == sizeof(mspHeaderV2_t) {
+                    // Copy header values into packet
+                    let mut header: HeaderV2 = m_inputBuffer[0];
+                    self.packet.payloadSize = header.payloadSize;
+                    self.packet.function = header.function;
+                    self.packet.flags = header.flags;
+                    // reset the offset iterator for re-use in payload below
+                    self.offset = 0;
+                    self.inputState = State::PAYLOAD_V2_NATIVE;
+                }
             }
-            break;
+            State::PAYLOAD_V2_NATIVE => {
+                // Read bytes until we reach payloadSize
+                self.offset += 1;
+                self.offset += 1;
+                self.packet.payload[self.offset] = c;
+                self.crc = crc8_dvb_s2(m_crc, c);
 
-        case MSP_PAYLOAD_V2_NATIVE:
-            // Read bytes until we reach payloadSize
-            m_packet.payload[m_offset++] = c;
-            m_crc = crc8_dvb_s2(m_crc, c);
-
-            // If we've received the correct amount of bytes for payload
-            if (m_offset == m_packet.payloadSize) {
-                // Then we're up to the CRC
-                m_inputState = MSP_CHECKSUM_V2_NATIVE;
+                // If we've received the correct amount of bytes for payload
+                if m_offset == self.packet.payloadSize {
+                    // Then we're up to the CRC
+                    self.inputState = State::CHECKSUM_V2_NATIVE;
+                }
             }
-            break;
-
-        case MSP_CHECKSUM_V2_NATIVE:
-            // Assert that the checksums match
-            if (m_crc == c) {
-                m_inputState = MSP_COMMAND_RECEIVED;
+            State::CHECKSUM_V2_NATIVE => {
+                // Assert that the checksums match
+                if self.crc == c {
+                    self.inputState = State::COMMAND_RECEIVED;
+                } else {
+                    println!("CRC failure on MSP packet - Got %d expected %d", c, m_crc);
+                    self.inputState = State::IDLE;
+                }
             }
-            else {
-                DBGLN("CRC failure on MSP packet - Got %d expected %d", c, m_crc);
-                m_inputState = MSP_IDLE;
-            }
-            break;
 
-        default:
-            m_inputState = MSP_IDLE;
-            break;
+            _ => {
+                self.inputState = State::IDLE;
+            }
+        }
+
+        // If we've successfully parsed a complete packet
+        // return true so the calling function knows that
+        // a new packet is ready.
+        if self.inputState == State::COMMAND_RECEIVED {
+            return true;
+        }
+        return false;
     }
 
-    // If we've successfully parsed a complete packet
-    // return true so the calling function knows that
-    // a new packet is ready.
-    if (m_inputState == MSP_COMMAND_RECEIVED) {
-        return true;
+    fn getReceivedPacket(&self) -> Packet {
+        self.packet
     }
-    return false;
-}
 
-mspPacket_t*
-MSP::getReceivedPacket()
-{
-    return &m_packet;
-}
-
-void
-MSP::markPacketReceived()
-{
+    fn markPacketReceived(&mut self)    {
     // Set input state to idle, ready to receive the next packet
     // The current packet data will be discarded internally
-    m_inputState = MSP_IDLE;
-}
-
-bool
-MSP::sendPacket(mspPacket_t* packet, Stream* port)
-{
-    // Sanity check the packet before sending
-    if (packet->type != MSP_PACKET_COMMAND && packet->type != MSP_PACKET_RESPONSE) {
-        // Unsupported packet type (note: ignoring '!' until we know what it is)
-        return false;
+    self.inputState = State::IDLE;
     }
 
-    if (packet->type == MSP_PACKET_RESPONSE && packet->payloadSize == 0) {
-        // Response packet with no payload
-        return false;
-    }
+    // todo: This involves a serial write, I think. Probably not what we want?
+    // fn sendPacket(packet: Packet, Stream* port) -> bool {
+    //     // Sanity check the packet before sending
+    //     if packet.type_ != PacketType::COMMAND && packet.type_ != PacketType::RESPONSE {
+    //         // Unsupported packet type (note: ignoring '!' until we know what it is)
+    //         return false;
+    //     }
+    //
+    //     if packet.type_ == PacketType::RESPONSE && packet.payloadSize == 0 {
+    //         // Response packet with no payload
+    //         return false;
+    //     }
+    //
+    //     // Write out the framing chars
+    //     port.write('$');
+    //     port.write('X');
+    //
+    //     // Write out the packet type
+    //     if packet.type_ == PacketType::COMMAND {
+    //         port.write('<');
+    //     }
+    //     else if packet.type_ == PacketType::RESPONSE {
+    //         port.write('>');
+    //     }
+    //
+    //     // Subsequent bytes are contained in the crc
+    //     let mut crc: u8 = 0;
+    //
+    //     // Pack header struct into buffer
+    //     uint8_t headerBuffer[5];
+    //     mspHeaderV2_t* header = (mspHeaderV2_t*)&headerBuffer[0];
+    //     header.flags = packet->flags;
+    //     header.function = packet->function;
+    //     header.payloadSize = packet->payloadSize;
+    //
+    //     // Write out the header buffer, adding each byte to the crc
+    //     for (uint8_t i = 0; i < sizeof(mspHeaderV2_t); ++i) {
+    //         port.write(headerBuffer[i]);
+    //         crc = crc8_dvb_s2(crc, headerBuffer[i]);
+    //     }
+    //
+    //     // Write out the payload, adding each byte to the crc
+    //     for (uint16_t i = 0; i < packet->payloadSize; ++i) {
+    //         port.write(packet->payload[i]);
+    //         crc = crc8_dvb_s2(crc, packet->payload[i]);
+    //     }
+    //
+    //     // Write out the crc
+    //     port.write(crc);
+    //
+    //     return true;
+    // }
+    //
 
-    // Write out the framing chars
-    port->write('$');
-    port->write('X');
-
-    // Write out the packet type
-    if (packet->type == MSP_PACKET_COMMAND) {
-        port->write('<');
-    }
-    else if (packet->type == MSP_PACKET_RESPONSE) {
-        port->write('>');
-    }
-
-    // Subsequent bytes are contained in the crc
-    uint8_t crc = 0;
-
-    // Pack header struct into buffer
-    uint8_t headerBuffer[5];
-    mspHeaderV2_t* header = (mspHeaderV2_t*)&headerBuffer[0];
-    header->flags = packet->flags;
-    header->function = packet->function;
-    header->payloadSize = packet->payloadSize;
-
-    // Write out the header buffer, adding each byte to the crc
-    for (uint8_t i = 0; i < sizeof(mspHeaderV2_t); ++i) {
-        port->write(headerBuffer[i]);
-        crc = crc8_dvb_s2(crc, headerBuffer[i]);
-    }
-
-    // Write out the payload, adding each byte to the crc
-    for (uint16_t i = 0; i < packet->payloadSize; ++i) {
-        port->write(packet->payload[i]);
-        crc = crc8_dvb_s2(crc, packet->payload[i]);
-    }
-
-    // Write out the crc
-    port->write(crc);
-
-    return true;
 }
