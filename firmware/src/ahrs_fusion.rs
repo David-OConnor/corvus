92028c6e5b67789461aca1d9901347367081aa54
David O'Connor <david.alan.oconnor@gmail.com>
2022-07-03 17:01:22 -0400

    More work on H7 board

--- firmware/src/ahrs_fusion.rs
+++ firmware/src/ahrs_fusion.rs
@@ -438,8 +438,8 @@ impl Ahrs {
 
     /// todo WIP
     /// Not part of official AHRS fusion algorithm
-    fn apply_controls_fixed_wing(&mut self, controls: CtrlInputs) {
-
+    fn apply_controls_fixed_wing(&mut self, controls: CtrlInputs, airspeed: f32) {
+        let g_estimated = airspeed * controls.pitch; // downward G forces from a maneuver. Actually in m/s^2
     }
 }
 
